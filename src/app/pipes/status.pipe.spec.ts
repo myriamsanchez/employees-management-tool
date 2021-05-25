52721d666f588228c71d68792fa8e2761e41f20b
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-05-25 12:19:13 +0200

    added some tests for the status pipe

--- src/app/pipes/status.pipe.spec.ts
+++ src/app/pipes/status.pipe.spec.ts
@@ -1,8 +1,18 @@
 import { StatusPipe } from './status.pipe';
 
+const pipe = new StatusPipe();
+
 describe('StatusPipe', () => {
   it('create an instance', () => {
     const pipe = new StatusPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it ('transforms 1 to "Inactive"', () => {
+    expect(pipe.transform(1)).toBe('Inactive');
+  });
+
+  it ('transforms null to "unknown"', () => {
+    expect(pipe.transform(null)).toBe('unknown');
   });
 });
