8480e12e87287d561099ab9d97c14f179ee97d4b
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-05-30 20:53:59 +0200

    added primeflex to project,added more employees to mock

--- src/app/mocks/employees.ts
+++ src/app/mocks/employees.ts
@@ -21,7 +21,7 @@ export const EMPLOYEES: Employee[] = [
     id: 3,
     name: 'Bill',
     surname: 'Gates',
-    position: 4,
+    position: 0,
     birthDate: new Date(1955, 9, 28),
     status: 0
   },
@@ -31,6 +31,30 @@ export const EMPLOYEES: Employee[] = [
     surname: 'Curie',
     position: 4,
     birthDate: new Date(1967, 10, 7),
+    status: 0
+  },
+  {
+    id: 5,
+    name: 'Dian',
+    surname: 'Fossey',
+    position: 2,
+    birthDate: new Date(1936, 0, 16),
+    status: 0
+  },
+  {
+    id: 6,
+    name: 'Andreas',
+    surname: 'Vesalius',
+    position: 1,
+    birthDate: new Date(1954, 11, 31),
+    status: 0
+  },
+  {
+    id: 7,
+    name: 'Rosalyn',
+    surname: 'Yalow',
+    position: 5,
+    birthDate: new Date(1921, 6, 19),
     status: 0
   }
 ];
